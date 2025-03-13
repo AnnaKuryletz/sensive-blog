20b3aa8be1ac062a8cc359c5c50d231b12d33ebb
anyukurilets <anyu.kutilez@mail.ru>
2025-03-13 15:18:46 +0300

    cleanup the code

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 from blog.models import Post, Tag, Comment
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_at')
+    raw_id_fields = ('author', 'tags')
+    autocomplete_fields = ('likes',)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'published_at', 'text')
+    raw_id_fields = ('post', 'author')
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
